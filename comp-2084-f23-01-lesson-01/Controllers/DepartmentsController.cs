e0d9ef940f34754429c439357a8fe8e19b140492
altishab <113277305+altishab@users.noreply.github.com>
2023-09-13 17:50:22 -0400

    init

---
+++ comp-2084-f23-01-lesson-01/Controllers/DepartmentsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorldDominion
+{
+    public class DepartmentsController : Controller 
+    {
+        public IActionResult Index()
+        {
+            List<Dictionary<string, object>> data = new() {
+                new() { { "Id", "1" }, {"Name", "Fruits & Veg"} }, 
+                new() { { "Id", "2" }, {"Name", "Meats"} }, 
+                new() { { "Id", "3" }, {"Name", "Sweets and Treats"} }, 
+            };
+
+            return View(); 
+        } 
+    }
+}
